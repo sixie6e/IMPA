1c99cb273f5731819cc6f3d4aac440398597aeaf
Brent Elisens <37149480+sixie6e@users.noreply.github.com>
2024-10-29 16:26:50 -0400

    Update outline.js

--- outline.js
+++ outline.js
@@ -2,105 +2,134 @@
 // State -> Society/Facility -> Violent/Non -> Death/Injury -> Financial
 // sixie6e@distherapy 2024
 
-Alabama:
-	In Society:
-		Cameroun Tremble(police) - animal cruelty
-		Dana Elmore(police) - tased handcuffed man
-		Justin Bisharat(police) - domestic strangulation
-		Jason Hadaway(detective) - perjury
-		Michael Kilgore(police) - planting drugs
-		Christopher Dressel(police) - animal cruelty
-		John Barton(police) - arrested caller for no id
-		Noel Wood(marine officer) - misuse of confidential data
-		Johnny Brown(officer) - assault
-		Cornelius Robinson - negligent homicide		
-		Jerry Cannon(police) - misuse conifidential data, drugs
-		Justin Barley(chief) - permit drunk driving police to go home
-		Kelsey Quesenberry(police) - dv, endangering a child
-		Ronald Monroe Jr(police) - agg dv
-		Mark Anthony Whalen(police) - dv
-		Mac Bailey Marquette(police) - murder
-		Joddie Brooke King(parole) - agg child abuse/assault
-		Adam Grant Shipley(police) - intox, harassment, abuse of authority
-		Randy Ford Sumner(police) - b&e, assault
-		Kevin Walter(police) - sexual assault
-		Jarrod Webster(police) - rape, sodomy
-		Eric Parker(police) - assault
-		Joshua Matthew Davidson(police) - kidnapping, sex trafficking
-		Ryan Snow(police) - posted protesters in rifle scope
-		Josh Hardy(police) - stole Pokemon cards
-		Horton Owens(police) - child abuse
-		Aaron AC Smith(police) - murder
-		Christopher Looser(police) - agg assault disfigurement
-		Amber Bolt(police) - lied for Christoper Looser
-		Mercedes Rugart(police) - dui
-		Alfreda Fluker(police) - murder
-		Billy Maurice Driggers(chief) - masturbating in front of teens
-		Arthur Maudlin(parole) - dui
-		Chris Reed(police) - drugs
-		Troy Middlebrooks(police) - proposed murdering black man
-		Willy Robinson(chief) - defended Troy Middlebrooks
-		Deonte Hamner(police) - sodomy at gunpoint
-		Carson Hix(police) - dv
-		William Squires(police) - sexual assault of a minor
-		Jay Steward(police) - dui homicide
-		Paul Callegari(police) - beat handcuffed black man
-		Matthew Moore(police) - serial, violent sexual assault
-		James Kent(police) - excessive force
-		Phillip Champion(police) - excessive force
-		Gregory Pimm(police) - excessive force
-		Ryan Manning(police) - child abuse, dv
-		Daniel Chesser(police) - racist k9
-		Robert Golden(police) - assault, menacing, harassment
-		Christopher Allen Triplett(police) - rape of a minor
-		Christopher Eugene Sanspree Jr(police) - swatting
-		Ismael Roldan(police) - rape of child
-		William Henry Shields(corrections) - animal cruelty
-		Michael Jay Lewis(sheriff) - animal cruelty, attempted murder
-		Christopher Whalen(police) - manslaughter
-		James Fowler(ahp)- manslaughter
-		Charles Forbes(police) - murder
-		Brandon Shane Mundy(police) - negligent homicide
-		Andrew Bass(police) - negligent homicide
-		William Darby(police) - murder
-		
-	In Facility:
-		William Todd(corrections) - stole from inmate
-		Jesse Cobb(corrections) - beat inmate
-		Ryan Phillips(police) - beat inmate
-		Quindarius Thagard(corrections) - smuggling
-		Corey McClendon(corrections) - smuggling
-		Timothy Lee Scarbrough(corrections) - sodomy
-		Corey McClendon(corrections) - smuggling
-		Andrew Bailey(corrections) - smuggling
-		Charlie Townsend(corrections) - smuggling
-		Antwan Giles(corrections) - smuggling
-		Timothy Robinson(corrections) - corruption
-		Akeem Edmonds(corrections) - beat inmate with belt
-		Jeffrey Jackson(corrections) - smuggling
-		William Patrick(corrections) - smuggling
-		Essence Woods(corrections) - smuggling
-		Alex Andrews(corrections) - smuggling
-		Andrew Taylor Roy(corrections) - smuggling
-		John Paul Ketterman(corrections) - smuggling
-		Shamarion Dozier(corrections) - smuggling
-		Deaundra Johnson(corrections) - bribery
-		Centauria Olds(corrections) - bribery
-		Vicky White(corrections) - inmate escape
-		William Clemons(corrections) - smuggling
-
-		
-		
+Alabama
+	Cameroun Tremble(police) - animal cruelty
+	Dana Elmore(police) - tased handcuffed man
+	Justin Bisharat(police) - domestic strangulation
+	Jason Hadaway(detective) - perjury
+	Michael Kilgore(police) - planting drugs
+	Christopher Dressel(police) - animal cruelty
+	John Barton(police) - arrested caller for no id
+	Noel Wood(marine officer) - misuse of confidential data
+	Johnny Brown(officer) - assault
+	Cornelius Robinson - negligent homicide		
+	Jerry Cannon(police) - misuse conifidential data, drugs
+	Justin Barley(chief) - permit drunk driving police to go home
+	Kelsey Quesenberry(police) - dv, endangering a child
+	Ronald Monroe Jr(police) - agg dv
+	Mark Anthony Whalen(police) - dv
+	Mac Bailey Marquette(police) - murder
+	Joddie Brooke King(parole) - agg child abuse/assault
+	Adam Grant Shipley(police) - intox, harassment, abuse of authority
+	Randy Ford Sumner(police) - b&e, assault
+	Kevin Walter(police) - sexual assault
+	Jarrod Webster(police) - rape, sodomy
+	Eric Parker(police) - assault
+	Joshua Matthew Davidson(police) - kidnapping, sex trafficking
+	Ryan Snow(police) - posted protesters in rifle scope
+	Josh Hardy(police) - stole Pokemon cards
+	Horton Owens(police) - child abuse
+	Aaron AC Smith(police) - murder
+	Christopher Looser(police) - agg assault disfigurement
+	Amber Bolt(police) - lied for Christoper Looser
+	Mercedes Rugart(police) - dui
+	Alfreda Fluker(police) - murder
+	Billy Maurice Driggers(chief) - masturbating in front of teens
+	Arthur Maudlin(parole) - dui
+	Chris Reed(police) - drugs
+	Troy Middlebrooks(police) - proposed murdering black man
+	Willy Robinson(chief) - defended Troy Middlebrooks
+	Deonte Hamner(police) - sodomy at gunpoint
+	Carson Hix(police) - dv
+	William Squires(police) - sexual assault of a minor
+	Jay Steward(police) - dui homicide
+	Paul Callegari(police) - beat handcuffed black man
+	Matthew Moore(police) - serial, violent sexual assault
+	James Kent(police) - excessive force
+	Phillip Champion(police) - excessive force
+	Gregory Pimm(police) - excessive force
+	Ryan Manning(police) - child abuse, dv
+	Daniel Chesser(police) - racist k9
+	Robert Golden(police) - assault, menacing, harassment
+	Christopher Allen Triplett(police) - rape of a minor
+	Christopher Eugene Sanspree Jr(police) - swatting
+	Ismael Roldan(police) - rape of child
+	William Henry Shields(corrections) - animal cruelty
+	Michael Jay Lewis(sheriff) - animal cruelty, attempted murder
+	Christopher Whalen(police) - manslaughter
+	James Fowler(ahp)- manslaughter
+	Charles Forbes(police) - murder
+	Brandon Shane Mundy(police) - negligent homicide
+	Andrew Bass(police) - negligent homicide
+	William Darby(police) - murder
+	Joshua Jones(corrections) - negligent homicide, assault
+	William Todd(corrections) - stole from inmate
+	Jesse Cobb(corrections) - beat inmate
+	Ryan Phillips(police) - beat inmate
+	Quindarius Thagard(corrections) - smuggling
+	Corey McClendon(corrections) - smuggling
+	Timothy Lee Scarbrough(corrections) - sodomy
+	Corey McClendon(corrections) - smuggling
+	Andrew Bailey(corrections) - smuggling
+	Charlie Townsend(corrections) - smuggling
+	Antwan Giles(corrections) - smuggling
+	Timothy Robinson(corrections) - corruption
+	Akeem Edmonds(corrections) - beat inmate with belt
+	Jeffrey Jackson(corrections) - smuggling
+	William Patrick(corrections) - smuggling
+	Essence Woods(corrections) - smuggling
+	Alex Andrews(corrections) - smuggling
+	Andrew Taylor Roy(corrections) - smuggling
+	John Paul Ketterman(corrections) - smuggling
+	Shamarion Dozier(corrections) - smuggling
+	Deaundra Johnson(corrections) - bribery
+	Centauria Olds(corrections) - bribery
+	Vicky White(corrections) - inmate escape
+	William Clemons(corrections) - smuggling
 
 Alaska
+	Joseph Miller(state police) - assault, let dog bite for more than a minute
+	Jason Woodruff(state police) - assault, let dog bite for more than a minute
+	Scott Boneta(police) - sexual assault(numerous)
+	Jonathan Murphy(police) - assault
+	Ronald Monroe Jr(police) - agg dv
+	Monte LaVelle Chitty(police) - child sexual abuse
+	Ethan Copeland(police) - dui in police car
+	Andres Ornelas(police) - sexual abuse of teen
+	David Bower(police) - assault
+	Nimeron Mike(police) - sex crimes
+	Jordan Serafin(police) - assault, burglary
+	Aaron Pettus(police) - assault of bicyclist
+	Matthew Owens(police) - murder
+	Anthony Rollins(police) - rape
+	Patrick J OKane(corrections) - agg assault, dv
+	Peter Schneider(police) - solicitation of a minor
+	Casey Richardson(police) - poisoned wolves
+	Ron Hobson(police) - agg assault
+	Rafael Mora-Lopez(police) - identity theft
+	Garrett Butler(police) - assault
+	Peter Lombardo(police) - dui
+	Jalonni Blackshear(corrections) - rape of a minor, murder
+	Justin Andrews(corrections) - sexual assault
+	Adam Spindler(corrections) - smuggling
+	Fredrick Fangonilo(corrections) - sexual assault
+	Brianna Andrews(corrections) - smuggling
+	Bryson Hayes(corrections) - assault
+	Steven Manuel(corrections) - smuggling
+	Stewart Blakeslee(corrections) - child pornography
+
+
 Arizona
 	Richard Chrisman(police) - murder
 	Jill Houchens(police) - animal cruelty
+		
+	
 Arkansas
 	Coleman Brackney(police) - murder
 	Larry P. Norman(state police) - negligent homicide
 	Jerri Beyard(police) - animal cruelty
 	Michael Davis(sheriff) - negligent homimcide
+	
 California
 	John Swearengin(sheriff) - vehicular homicide
 	Johannes Mehserle(police) - murder
@@ -115,6 +144,8 @@ California
 	Aaron Russell(sheriff) - murder
 	Benito Soto(police) - vehicular homicide
 	Benjamin Jillson(police) - vehicular homicide
+
+	
 Colorado
 	James Ashby(police) - stalking, murder
 	Adam Lunn(sheriff) - murder
@@ -122,17 +153,28 @@ Colorado
 	Randy Roedema(police) - murder
 	Kyle Alan Gould(sheriff) - manslaughter
 	Andrew Buen(sheriff) - manslaughter
+	
 Connecticut
 	Jason Anderson(police) - negligent homicide
 	Scott Byron Smith(police) - murder
 	William Scribner(police) - murder
 	Brendan Borer(police) - removed tents of homeless
+	
+	
 Delaware
+	Dempsey Walters(state police) - assaulted teens
+	Samuel Walters(police) - agg assault
+	Jahee White(corrections) - smuggling
+	Kyle Linville(police) - intimidation with gun
+	Angela Dildy(corrections) -  smuggling
+	Michael Coleman(police) - PPP fraud
+
 DC
 	Reginald Jones(police) - robbery, murder
 	Andrew Zabavsky(polcie) - manslaughter
 	Terence Sutton(police) - manslaughter
 	Enis Jevric(police) - murder
+	
 Florida
 	John Rutherford(corrections) - animal cruelty
 	Robert Koenig(police) - manslaughter
@@ -152,18 +194,54 @@ Georgia
 	Jason Kenny(sheriff) - torture, murder
 	Robert Olsen(police) - murder
 Hawaii
+	Mark Kealoha Kaili Jr(police) - assault, burglary
+	William Gary(dle) - racial harassment
+	William K Keahi(sheriff) - harassment, endangerment
+	Eric Mitamura(ccd) - racial harassment
+	Alvin Turla(ccd) - racial harassment
 Idaho
+	Gordon Shaw(police) - human trafficking
+	Bradley Chambers(police) - excessive force
+	Ryan Bendawald(police) - sexual assault using authority
+	Daniel Howard(state police) - murder
+	Saif Sabah Hasan Al Anbagi (parole) - sexual extortion of parolee
+	Donald Heida(officer) - agg assault
+
 Illinois
 	Jason Van Dyke(police) - manslaughter
 	William McKinney(police) - manslaughter
 	Matt Mitchell(state police) - manslaugher
 	John McDonald(police) - murder
+	
 Indiana
 	Kyle James Vincent(police) - animal cruelty
+	Jaydan Vanosdol(police) - child pornography
+	James W Bond(police) - solicitation of a minor
+	Ryan Kijurna(police) - dv
+	Javed Richards(police) - child exploitation
+	Kamal Bola(police) - child molestation
+	
 Iowa
+	Mike Tobin(police) - sexual exploitation/sol of minor
+	Tyler Breitbach(police) - dv
+	Mario Antonio Rocha(corrections) - smuggling
+	Juan D Lopez(police) - poss w/ intent
+	Payton L Boston(police) - dui
+
+
 Kansas
+	Gideon Cody(police) - obstruction, witness tampering
+	Adam Vendetti(police) - assault of child
+	Russell Patterson Jr(police) - human trafficking, rape
+	David Justice(police) - child sex crimes
+
 Kentucky
 	Bobby Joe Smith(constable) - murder
+	Javar Downs(police) - theft
+	Mickey Shawn Stines(sheriff) - murder
+	Rachael Curnutte(police) - dui
+	Tanner M Abbott(sheriff) - abuse of detainees, forged paperwork
+	
 Louisiana
 	Norris Greenhouse Jr(police) - killed 6 year old
 	Derrick Stafford(police) - killed 6 year old
@@ -175,20 +253,36 @@ Louisiana
 	Robert Faulcon Jr(police) - murder
 	Melvin Williams(police) - murder
 	David Warren(police) - murder
+	Kory York(police) - murder
+	Chris Hollingsworth(police) - murder
+	John Clary(police) - murder
+	Dakota DeMoss(police) - murder
+	
 Maine
 	Addison Cox(police) - beat porcupines to death for snapchat
 	Michael Rolerson(police) - beat porcupines to death for snapchat
+	Larry Fickett(police) - sexual exploitation of a child
+	Chandler Cole(police) - agg forgery, falsifying evidence
+	James Walter Best Jr(police) - child sex crimes
+	Zahra Munye Abu(police) - assault
+	Stephen Easley Jr(police) - dv
+	Jeffrey Bishop(police) - drug trafficking
+	Robert Ireland(corrections) - sexual assault, smuggling
+	
 Maryland
 	Robert Warrington(police) - attempted murder
 	David Acosta(police) - animal cruelty(his k9)
+	
 Massachusetts
 	Stuart Merry(police) - negligent homicide
 	James Hall(police) - manslaughter 
+	
 Michigan
 	Kenneth Bluew(police) - murdered pregnant woman
 	Larry Nevers(police) - murder
 	Walter Budzyn(police) - murder
 	Mark Bessner(state police) - manslaughter
+	
 Minnesota
 	Robert leroy Nelson(state police) - rape, torture, murder
 	Mohamed Noor(police) -  murder
@@ -198,8 +292,10 @@ Minnesota
 	Tou Thao(police) - murder
 	Kim Potter(police) - murder
 	Brian Cummings(police) - vehicular homicide
+	
 Mississippi
 	Anthony Fox(police) - murder
+	
 Missouri
 	Anthony Piercy(mhp) - negligent homicide
 	Brian Massa(police) - manslaughter
@@ -209,32 +305,43 @@ Missouri
 	Andria Heese(police) - vehicular manslaughter
 	Nathaniel Hendren(police) - murder
 	Eric DeValkenaere(police) - murder, planting evidence
+	
 Montana
+
 Nebraska
+
 Nevada
 	Joshua Corcran(state police) - murder
+	
 New Hampshire
+
 New Jersey
 	Jovanny Crespo(police) - murder
+	
 New Mexico
 	Billy Anders(sheriff) - manslaughter
 	Joseph Mantelli(police) - murder
+	
 New York
-	In Society:
-		Brian Conroy(police) - murder
-		Thomas Ryan(police) - murder
-		Jonas Bright(police) - murder
-		Paolo Colecchia(police) - murder
-		Peter Liang(police) - murder	
-	In Facility:
-		Rebecca Hillman(corrections) - negligent homicide
+	Brian Conroy(police) - murder
+	Thomas Ryan(police) - murder
+	Jonas Bright(police) - murder
+	Paolo Colecchia(police) - murder
+	Peter Liang(police) - murder	
+	Rebecca Hillman(corrections) - negligent homicide
+	
 North Carolina
 	Phillip Barker(police) - manslaughter
 	Jesse Santifort (police) - murder
+	
 North Dakota
+
 Ohio
+	Adam Coy(police) - murder
 	Jeffrey Hodge(police) - murder
 	Cecil Morrison(police) - negligent homicide
+	Adrian Wilson(police) - let k9 maul
+	
 Oklahoma
 	Keith Sweeney(police) - murder
 	Randy Trent Harrison(police) - murder
@@ -243,7 +350,9 @@ Oklahoma
 	Robert Charles Bates(sheriff) - murder
 	Brandon Dingman(police) - murder
 	Joshua Taylor(police) - murder
+	
 Oregon
+
 Pennsylvania
 	Steven Homanko(police) - vehicular homicide
 	Matthew J. Hinkel(police) - manslaughter
@@ -257,14 +366,18 @@ Pennsylvania
 	Sean Dolan(police) - manslaughter
 	Devon Smith(police) - manslaughter
 	Edsaul Mendoza(police) - murder
+	
 Rhode Island
 	Joseph Warzycha(police) - manslaughter
+	
 South Carolina
 	Justin Craven(police) - murder
 	Richard Combs(police) - murder
 	Michael Slager(police) - murder
 	Stephen Flood(sheriff) - vehicular homicide
+	
 South Dakota
+
 Tennessee
 	Paul Carrier(police) - reckless homicide
 	Desmond Mills(police) - murder, kidnapping
@@ -274,6 +387,8 @@ Tennessee
 	Justin Smith(police) - murder, kidnapping
 	Mike Holmes(sheriff) - manslaughter
 	Andrew Delke(police) - murder
+	Sean Herman(police) - onlyfans
+	
 Texas
 	Jason Blackwelder(police) - manslaughter
 	Teddie Whitefield(police) - dui, homicide
@@ -286,9 +401,12 @@ Texas
 	Bau Tran(police) - murder
 	Gerald Goines(police) - fake warrant, murder
 	Aaron Dean(police) - murder
+	
 Utah
 	Joseph Corbett(police) - manslaughter
+	
 Vermont
+
 Virginia
 	Adam D. Torres(police) - murder
 	Daniel Harmon Wright(police) - murder
@@ -297,6 +415,7 @@ Virginia
 	Stephen Rankin(police) - murder
 	Albin Pearson(police) - murder
 	Richard Daniel Johnson(police) - vehicular homicide
+	
 Washington
 	Karl Thompson(police) - murder
 	Mathias Bachmeier(sheriff) - murder
@@ -304,11 +423,14 @@ Washington
 	W.F. Stephenson(police) - manslaughter
 	Fred Paschal(police) - manslaughter
 	Jeffrey Nelson(police) - murder
+	
 West Virginia
 	Brandon Tagayun(police) - murder
 	Travis Snuffer(chief) - animal cruelty
+	
 Wisconsin
 	Thomas Grady(police) - murder
 	John Mueller(police) - murder
 	Joel Streicher(sheriff) - vehicular homicide
+	
 Wyoming
